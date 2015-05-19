da021582cb0aaafd40c82709d29b383318f2520c
JRocks247 <allen_jono@yahoo.ca>
2015-05-19 16:25:50 -0600

    changes

--- Pollutantmean.R
+++ Pollutantmean.R
@@ -1,48 +1,46 @@
 pollutantmean <- function(directory, pollutant, id = 1:332) {
   
- 	setwd("C:/Users/Jono/Desktop/Coursera/specdata") 
-  	total = 0                            
-  	observations = 0                     
+	setwd("C:/Users/Jono/Desktop/Coursera/specdata") 
+		total = 0                            
+			observations = 0                     
   
-   
-  for (i in id)
+  
+				for (i in id)
 {
-             
-      if (i <10) { 
+            
+if (i <10) { 
                          data <- read.csv(paste("0","0", as.character(i), ".csv", sep=""),  
-                                        header = T, 
-                                        na.strings=c("NA","NaN", " "))
+                         	header = T, 
+                                	na.strings=c("NA","NaN", " "))
                  }
       
 else if (i>=10 & i<100) { 
                          data <- read.csv(paste("0", as.character(i), ".csv", sep=""),  
-                                          header = T, 
-                                          na.strings=c("NA","NaN", " ") 
+                         	header = T, 
+                         		na.strings=c("NA","NaN", " ") 
                                           )
                         }
                      
        
 
-     else       { 
+else       { 
                         data <- read.csv(paste(as.character(i), ".csv", sep=""),     
-                                         header = T, 
-                                         na.strings=c("NA","NaN", " ") 
+                        	header = T, 
+                        		na.strings=c("NA","NaN", " ") 
                                         )
                 }
   
-   		data = na.omit(data)    
- 
-	 		observations = observations + nrow(data)
- 
- if (pollutant == "sulfate") {total = total + sum(data$sulfate)}
- 
+data = na.omit(data)    
+	observations = observations + nrow(data)
+
+if (pollutant == "sulfate") {total = total + sum(data$sulfate)}
 
 else {total = total + sum(data$nitrate)}
 
 }
   
     setwd("..")
-  
+
   return (total/observations)
 
 }
